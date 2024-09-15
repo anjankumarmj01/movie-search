5957d2555776ed0d849eca514318f697f5999607
Anjan Kumar M J <kumar.anjan76@gmail.com>
2024-09-15 21:21:30 +0530

    minimum character error message and handled search issues

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,6 +1,16 @@
 import { useCallback, useEffect, useState } from 'react';
 import { useSearchMoviesQuery } from '../store/api';
-import { Input, Card, Row, Col, Button, Modal, Typography, Result } from 'antd';
+import {
+  Input,
+  Card,
+  Row,
+  Col,
+  Button,
+  Modal,
+  Typography,
+  Result,
+  Badge,
+} from 'antd';
 import { Link } from 'react-router-dom';
 import { Movie } from '../types/types';
 import useOnlineStatus from '../utils/useOnlineStatus';
@@ -20,6 +30,7 @@ const { Text } = Typography;
 const SearchPage = () => {
   const [isModalVisible, setIsModalVisible] = useState(false);
   const [addedMovieTitle, setAddedMovieTitle] = useState('');
+  const [showError, setShowError] = useState(false); // state for managing error display
   const dispatch = useDispatch();
 
   // Selectors
@@ -32,13 +43,13 @@ const SearchPage = () => {
 
   // Fetch movies based on search term
   const { data, isLoading, error } = useSearchMoviesQuery(searchTerm, {
-    skip: searchTerm.length === 0,
+    skip: searchTerm.length < 3,
   });
 
   // Function to dispatch search results based on API response
   const dispatchSearchResults = useCallback(() => {
     if (data) {
-      if (data.Response === 'True') {
+      if (data?.Response === 'True') {
         dispatch(setSearchResults(data));
       } else {
         dispatch(
@@ -59,7 +70,12 @@ const SearchPage = () => {
 
   // Handle search term input
   const handleSearch = (value: string) => {
-    dispatch(setSearchTerm(value));
+    if (value.length >= 3) {
+      dispatch(setSearchTerm(value));
+      setShowError(false);
+    } else {
+      setShowError(true);
+    }
   };
 
   // Handle adding movie to favourites
@@ -83,104 +99,87 @@ const SearchPage = () => {
           View Favourites
         </Button>
       </Link>
-      <Search
-        placeholder="Search movies by title"
-        allowClear
-        onSearch={handleSearch}
-        style={{ width: 400, marginBottom: 20 }}
-      />
-      {isLoading ? (
-        <Shimmer count={12} type="search" />
-      ) : error ? (
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <Search
+          placeholder="Search by title (enter minimum of 3 characters)"
+          allowClear
+          onSearch={handleSearch}
+          style={{ width: 400, marginBottom: 20 }}
+        />
+        {showError && (
+          <Badge
+            count="Please enter at least 3 characters to search"
+            style={{
+              backgroundColor: '#f5222d',
+              color: '#fff',
+              marginLeft: '10px',
+              borderRadius: '12px',
+              padding: '4px 12px',
+            }}
+          />
+        )}
+      </div>
+      {isLoading && <Shimmer count={12} type="search" />}
+      {error && (
         <Result
           status="warning"
-          title="Error occured while fetching movie details"
+          title="Error occurred while fetching movie details"
         />
-      ) : (
-        <>
-          {searchTerm === '' ? (
-            <div
-              style={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                height: '60vh',
-                textAlign: 'center',
-              }}
-            >
-              <Result
-                status="success"
-                title="Your searched movies appear here"
-                subTitle="Please start searching"
-              />
-            </div>
-          ) : searchResults?.length === 0 ? (
-            <div
-              style={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                height: '60vh',
-                textAlign: 'center',
-              }}
-            >
-              {searchTerm.length < 3 ? (
-                <Result
-                  title={`Too many results for "${searchTerm}".`}
-                  subTitle="Please type a minimum of three characters."
-                />
-              ) : (
-                <Result
-                  title={`No movies found matching "${searchTerm}".`}
-                  subTitle="Please try a different title."
-                />
-              )}
-            </div>
-          ) : (
-            <Row gutter={[16, 16]}>
-              {searchResults?.map((movie: Movie) => (
-                <Col key={movie.imdbID} xs={24} sm={12} md={8} lg={4}>
-                  <Link to={`/movie/${movie.imdbID}`}>
-                    <div style={{ width: '200px', height: '400px' }}>
-                      <Card
-                        hoverable
-                        cover={
-                          <img
-                            alt={movie.Title}
-                            src={movie.Poster}
-                            style={{ height: '300px', objectFit: 'cover' }}
-                          />
-                        }
-                        style={{
-                          width: '100%',
-                          height: '100%',
-                          overflow: 'hidden',
-                        }}
-                      >
-                        <Meta
-                          title={getDisplayValue(movie?.Title)}
-                          description={getDisplayValue(
-                            movie?.Year?.split('–')[0]
-                          )}
-                        />
-                      </Card>
-                    </div>
-                  </Link>
-                  <Button
-                    type="primary"
-                    style={{ marginTop: 10, width: '200px' }}
-                    onClick={() => handleAddToFavourites(movie)}
-                    disabled={favouriteIds.includes(movie.imdbID)}
+      )}
+      {searchTerm === '' && searchResults?.length === 0 && (
+        <Result
+          status="success"
+          title="Your searched movies appear here"
+          subTitle="Please start searching"
+        />
+      )}
+      {searchTerm && searchTerm?.length >= 3 && searchResults?.length === 0 && (
+        <Result
+          title={`No movies found matching "${searchTerm}".`}
+          subTitle="Please try a different title."
+        />
+      )}
+      {searchTerm && searchTerm?.length >= 3 && searchResults?.length > 0 && (
+        <Row gutter={[16, 16]}>
+          {searchResults?.map((movie: Movie) => (
+            <Col key={movie.imdbID} xs={24} sm={12} md={8} lg={4}>
+              <Link to={`/movie/${movie.imdbID}`}>
+                <div style={{ width: '200px', height: '400px' }}>
+                  <Card
+                    hoverable
+                    cover={
+                      <img
+                        alt={movie.Title}
+                        src={movie.Poster}
+                        style={{ height: '300px', objectFit: 'cover' }}
+                      />
+                    }
+                    style={{
+                      width: '100%',
+                      height: '100%',
+                      overflow: 'hidden',
+                    }}
                   >
-                    {favouriteIds.includes(movie.imdbID)
-                      ? 'Already in Favourites'
-                      : 'Add to Favourites'}
-                  </Button>
-                </Col>
-              ))}
-            </Row>
-          )}
-        </>
+                    <Meta
+                      title={getDisplayValue(movie?.Title)}
+                      description={getDisplayValue(movie?.Year?.split('–')[0])}
+                    />
+                  </Card>
+                </div>
+              </Link>
+              <Button
+                type="primary"
+                style={{ marginTop: 10, width: '200px' }}
+                onClick={() => handleAddToFavourites(movie)}
+                disabled={favouriteIds.includes(movie.imdbID)}
+              >
+                {favouriteIds.includes(movie.imdbID)
+                  ? 'Already in Favourites'
+                  : 'Add to Favourites'}
+              </Button>
+            </Col>
+          ))}
+        </Row>
       )}
 
       <Modal
