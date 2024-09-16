74d30fa74d71130db5187c73e4bac67d6ca72be7
Anjan Kumar M J <kumar.anjan76@gmail.com>
2024-09-16 12:28:31 +0530

    removed retry button from offline to improve user behaviour

--- src/components/Offline/OfflineMessage.tsx
+++ src/components/Offline/OfflineMessage.tsx
@@ -1,18 +1,11 @@
-import { Result, Button } from 'antd';
+import { Result } from 'antd';
 import { WifiOutlined } from '@ant-design/icons';
-import { RETRY } from '../../constants/constants';
-import { handleRetry } from '../../utils/utils';
 
 const OfflineMessage = () => (
   <Result
     icon={<WifiOutlined style={{ color: '#FF4D4F' }} />}
     title="No Internet Connection"
     subTitle="It seems like you are offline. Please check your internet connection and try again."
-    extra={
-      <Button type="primary" onClick={handleRetry}>
-        {RETRY}
-      </Button>
-    }
   />
 );
 
